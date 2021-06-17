62acb398bff2fdd7925bdd8b8866d15924e6209b
Spaceodist01 <elijahangell@outlook.com>
2021-06-17 18:37:40 -0400

    47 task done

--- index.js
+++ index.js
@@ -166,7 +166,10 @@ class Airplane {
        return `${this.favSubjects}`
      }
      PRAssignment(subject){
-        return `student.name has submitted a PR for ${subject}`
+        return `${this.name} has submitted a PR for ${subject}`
+     }
+     sprintChallenge(subject){
+       return `${this.name} has begun sprint challenge on ${subject}`
      }
  }
   
@@ -183,8 +186,18 @@ class Airplane {
           + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
           + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
   */
- class ProjectManager {
-     
+ class ProjectManager extends Instructor {
+     constructor(attributes){
+       super(attributes);
+       this.gradClassName = attributes.gradClassName;
+       this.favInstructor = attributes.favInstructor;
+     }
+     standUp(channel){
+       return `${this.name} announces to ${channel}, @channel standy times!`
+     }
+     debugsCode(subject){
+      return `${this.name} debugs ${this.name}'s code on ${subject}`
+     }
  }
   /*
     STRETCH PROBLEM (no tests!)
